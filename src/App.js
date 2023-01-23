082e0838ca3dd2b00c9633a6a86d3ac95401369d
OlaOluwalekan <olalekanbello534@gmail.com>
2023-01-23 14:05:42 +0100

    completed stripe menu/submenu

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Hero from './Hero'
+import Submemu from './Submenu'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submemu />
+    </>
+  )
+}
+
+export default App
